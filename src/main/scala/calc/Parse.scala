f431a24b9e6b179e63b28309f91d912f179e8822
gilc <cohengil3@gmail.com>
2021-06-29 16:43:40 +0300

    Remove warnings

--- src/main/scala/calc/Parse.scala
+++ src/main/scala/calc/Parse.scala
@@ -1,14 +1,13 @@
 package calc
 
 import scala.collection.mutable
-import scala.collection.mutable.Stack
 
 /*
 This module responsibility is to parse the input string and generate the AST
  */
 object Parse {
 
-  private def addNode(op: Op, stack: Stack[Expr]): Unit = {
+  private def addNode(op: Op, stack: mutable.Stack[Expr]): Unit = {
     assert(stack.nonEmpty, "Expression stack shouldn't be empty")
     val rightSon = stack.pop()
     assert(stack.nonEmpty, "Expression stack shouldn't be empty")
@@ -30,13 +29,13 @@ object Parse {
   }
 
   def parse(str: String): Expr = {
-    val exprStack: Stack[Expr] = new mutable.Stack[Expr]()
-    val opStack: Stack[Option[Op]] = new mutable.Stack[Option[Op]]()
+    val exprStack: mutable.Stack[Expr] = new mutable.Stack[Expr]()
+    val opStack: mutable.Stack[Option[Op]] = new mutable.Stack[Option[Op]]()
 
     def precedenceLoop(o1: Op): Unit = {
       while (opStack.nonEmpty) {
-        var o2: Op = opStack.top.getOrElse(return ())
-        if ((o1.isLeftAssoc()) && (o1.precedence() <= o2.precedence())) {
+        val o2: Op = opStack.top.getOrElse(return ())
+        if (o1.isLeftAssoc() && (o1.precedence() <= o2.precedence())) {
           opStack.pop()
           addNode(o2, exprStack)
         }
@@ -49,21 +48,19 @@ object Parse {
         case ' ' => ()
         case d if d.isDigit => exprStack.push(Number(d.asDigit))
         case '(' => opStack.push(None)
-        case d if isRParens(d) => {
+        case d if isRParens(d) =>
           while (opStack.top.isDefined) {
             addNode(opStack.pop().get, exprStack)
           }
-        }
-        case _ => {
+        case _ =>
           val o1: Op = parseOp(c).get
           precedenceLoop(o1)
           opStack.push(parseOp(c))
-        }
       }
     }
 
     str.foreach(i => handleChar(i))
-    val finalOpStack: Stack[Op] = opStack.flatten
+    val finalOpStack: mutable.Stack[Op] = opStack.flatten
     while (finalOpStack.nonEmpty) {
       addNode(finalOpStack.pop(), exprStack)
     }
